367d7ea6c4ae174934356cd5df566a1b47d61871
startcoding0to1 <startcoding0to1@gmail.com>
2026-02-22 19:25:37 +0530

    Test-2

--- portfolio-ui/src/app/app.ts
+++ portfolio-ui/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('portfolio-ui');
+  name = '';
 }
